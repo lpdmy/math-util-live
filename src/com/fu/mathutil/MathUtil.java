85864b76025c94e7299df3fcdf8e8a9db38cc371
lpdmy <dieumy430@gmail.com>
2024-05-01 18:54:15 +0700

    Lan dau dua code len Server

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,18 @@
+package com.fu.mathutil;
+
+public class MathUtil {
+
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid argument. n must be between 0... 20");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long product = 1;
+        for (int i = 2; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+}
---
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -0,0 +1,27 @@
+package com.fu.mathutil;
+
+/**
+ *
+ * @author lpdie
+ */
+public class MathUtilLive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long result = MathUtil.getFactorial(5);
+        System.out.println("5! = 120; actual: " + result);
+
+        System.out.println("6! = 720; actual: " + MathUtil.getFactorial(6));
+
+        System.out.println("0! = 1; actual: " + MathUtil.getFactorial(0));
+
+//        MathUtil.getFactorial(-5); ném ngoại lệ thật rồi
+        // kĩ thuật kiểm thử phần mềm : ước lượng xem giá trị trả về của hàm là gì 
+        // expected value: 5! = 120
+        // giá trị khi chạy thực tế là mấy - actual, giả sử 120
+        // if expected == actual, hàm ngon trong tình huống này
+    }
+
+}
